9c98c7899bfde539af13d1f4e9036c0c8025c23d
Sistemas Flota <sistemas@flotaoccidental.com>
2025-05-17 16:13:23 -0500

    Anterior datos en csv

--- limpiar_y_guardar.py
+++ limpiar_y_guardar.py
@@ -3,14 +3,17 @@
 import pandas as pd
 from limpieza import normalizar_dataset
 
+# Cargar archivo original
 archivo_entrada = 'Zonas.csv'
 archivo_salida = 'Zonas_limpio.csv'
 
-print(f"🧼 Cargando datos desde: {archivo_entrada}")
-df = pd.read_csv(archivo_entrada, encoding='latin1')
+print(f"Cargando datos desde: {archivo_entrada}")
+df = pd.read_csv( archivo_entrada , encoding='utf-8', delimiter=',', thousands=',', decimal='.')
+
+# Aplicar limpieza
 df_limpio = normalizar_dataset(df)
 
-# Guardar usando punto y coma como delimitador para evitar problemas con comas internas
-df_limpio.to_csv(archivo_salida, index=False, sep=';', encoding='utf-8')
+# Guardar archivo limpio
+df_limpio.to_csv(archivo_salida, index=False, encoding='utf-8')
 
-print(f"✅ Limpieza completada. Archivo guardado como: {archivo_salida}")
+print(f"\n✅ Limpieza completada. Archivo guardado como: {archivo_salida}")
